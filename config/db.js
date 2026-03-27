7ddccb7f8ac3fee59861c9bd64acea2525e39088
bobbygour30 <bobby30gour@gmail.com>
2026-03-27 18:33:39 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ const connectDB = async () => {
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
-    throw err; // ❗ DO NOT exit process
+    throw err;
   }
 };
 
